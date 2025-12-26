2e28eb864ebbd68fca7746314199d676f4b3d44a
Tommy Barnes <tommy.barnes@flocksafety.com>
2025-12-26 01:01:44 -0600

    Completed Day 1. Refactor input parsing and update spin lock logic.

--- day01/main.go
+++ day01/main.go
@@ -8,76 +8,68 @@ import (
 )
 
 // ParseInput reads and parses the input file
-func ParseInput(filename string) ([]int, error) {
+func parseInput(filename string) ([]string, []int, error) {
 	data, err := os.ReadFile(filename)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
-	
+
 	lines := strings.Split(strings.TrimSpace(string(data)), "\n")
 	numbers := make([]int, 0, len(lines))
-	
+	directions := make([]string, 0, len(lines))
+
 	for _, line := range lines {
+		dir := string(line[0])
+		directions = append(directions, dir)
+		line = line[1:]
+
 		num, err := strconv.Atoi(strings.TrimSpace(line))
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
+
 		numbers = append(numbers, num)
 	}
-	
-	return numbers, nil
+
+	return directions, numbers, nil
 }
 
-// Part1 solves part 1 of the puzzle - sum of all numbers
-func Part1(numbers []int) (interface{}, error) {
-	sum := 0
-	for _, num := range numbers {
-		sum += num
+// SpinLock simulates the spin lock and calculates the password
+func spinLock(directions []string, numbers []int) (int, error) {
+	password := 0
+	curr := 50
+	denom := 100
+	for i, num := range numbers {
+		dir := directions[i]
+		if dir == "L" {
+			curr = (denom + curr - num) % denom
+		} else if dir == "R" {
+			curr = (curr + num) % denom
+		}
+		if curr == 0 {
+			password++
+		}
 	}
-	return sum, nil
-}
-
-// Part2 solves part 2 of the puzzle - product of all numbers
-func Part2(numbers []int) (interface{}, error) {
-	product := 1
-	for _, num := range numbers {
-		product *= num
-	}
-	return product, nil
+	return password, nil
 }
 
 func main() {
 	// Test with example input
-	exampleData, err := ParseInput("example.txt")
+	directions, numbers, err := parseInput("example.txt")
 	if err != nil {
 		fmt.Printf("Error reading example.txt: %v\n", err)
-	} else {
-		result1, _ := Part1(exampleData)
-		fmt.Printf("Example Part 1: %v\n", result1)
-		result2, _ := Part2(exampleData)
-		fmt.Printf("Example Part 2: %v\n", result2)
 	}
 
-	fmt.Println()
+	password, _ := spinLock(directions, numbers)
+	fmt.Printf("Example password: %d\n", password)
 
 	// Solve with real input
-	data, err := ParseInput("input.txt")
+	directions, numbers, err = parseInput("input.txt")
 	if err != nil {
 		fmt.Printf("Error reading input.txt: %v\n", err)
 		os.Exit(1)
 	}
 
-	result1, err := Part1(data)
-	if err != nil {
-		fmt.Printf("Error in Part 1: %v\n", err)
-	} else {
-		fmt.Printf("Part 1: %v\n", result1)
-	}
-
-	result2, err := Part2(data)
-	if err != nil {
-		fmt.Printf("Error in Part 2: %v\n", err)
-	} else {
-		fmt.Printf("Part 2: %v\n", result2)
-	}
+	password, _ = spinLock(directions, numbers)
+	fmt.Printf("Input password: %d\n", password)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/tbarnes94/advent_of_code_25
 
-go 1.24.11
+go 1.24.4
